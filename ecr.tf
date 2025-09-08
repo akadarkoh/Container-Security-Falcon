219e24e12fc61510833b3f7eec2f88ccb62838e6
Kofi Asante <99542500+akadarkoh@users.noreply.github.com>
2025-09-08 16:29:09 -0400

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,8 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_ecr_repository" "foo" {
   name                 = "bar"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 }
